4a081ddcaa5994d8d51ea556c407621c82eaf529
MattSainz <Matthias.Sainz@colorado.edu>
2015-12-08 20:54:45 -0700

    update for heroku

--- server.js
+++ server.js
@@ -29,6 +29,8 @@ rl.on('close', function(){
     
   var app = express();
 
+  app.set('port', (process.env.PORT || 5000));
+
   app.use(express.static('public'));
 
   app.get('/', function(req, res){
@@ -43,8 +45,9 @@ rl.on('close', function(){
     });
   });
 
-  app.listen(3000);
-  console.log("Server is running on port 3000");
+  app.listen(app.get('port'), function(){
+    console.log("Server is running on port", app.get('port'));
+  });
 
 });
 
